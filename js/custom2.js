b298f702c16d5ce9a47f76465b5b5f1ef6ca5e04
Giovanni Canella <gi.canella85@outlook.com>
2023-12-04 02:17:20 -0300

    feitas mais algumas paginas

---
+++ js/custom2.js
@@ -0,0 +1,7 @@
+const caixaDialogo = document.getElementById("caixa-dialogo");
+const botaoConcordo = document.getElementById("botao-concordo");
+
+botaoConcordo.addEventListener("click", function () {
+  caixaDialogo.style.display = "none";
+  // Adicione aqui o código para marcar que o usuário concordou com os termos.
+});
